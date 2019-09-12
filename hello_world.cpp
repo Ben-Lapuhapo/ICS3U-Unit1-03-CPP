ff7ef3596823c07ea8ad706783e7f60e3b6deed1
Ben Lapuhapo <ben.lapuhapo@mths.ca>
2019-09-12 01:11:10 +0000

    Added Hello, World! program

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,5 +1,5 @@
 // Copyright (c) 2019 St. Mother Teresa HS All Rights Reserved.
-// 
+//
 // Created By: Ben Lapuhapo.
 // Created on: Sep 2019.
 // This is the "Hello, World!" program, but in proper format!
